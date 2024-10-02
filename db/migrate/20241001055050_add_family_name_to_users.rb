13887ffffd4c7dcd38260ca2b473b19be19b449b
lazykapo <sideorder921@gmail.com>
2024-10-02 11:34:11 +0900

    Revert "usersテーブルカラム追加"
    This reverts commit 3168ab019281000e62c3477539c7e93ae3d49d0e.

--- db/migrate/20241001055050_add_family_name_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddFamilyNameToUsers < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users, :family_name, :string
-  end
-end
--- db/migrate/20241001060151_add_family_name_kana_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddFamilyNameKanaToUsers < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users, :family_name_kana, :string
-  end
-end
