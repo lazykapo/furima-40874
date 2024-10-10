52348eb63e0b2291cd3c8f5f76557e0720d557cf
lazykapo <sideorder921@gmail.com>
2024-10-10 15:49:17 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,4 +5,10 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  private
+
+  def image_params
+    params.require(:image).permit(:content, :image).merge(user_id: current_user.id)
+  end
 end
