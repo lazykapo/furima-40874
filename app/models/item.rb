6487e34cbfe96fe6fc17d1a298e887fed39de11d
lazykapo <sideorder921@gmail.com>
2024-10-11 15:57:04 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,21 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one :order
   has_one_attached :image
+
+  # ここから下は商品名と内容が画像に必須かどうか
+  # validates :name, presence: true, unless: :was_attached?
+  # validates :description, presence: true, unless: :was_attached?
+
+  # def was_attached?
+  # image.attached?
+  # end
+
+  # Itemモデルのアソシエーションを設定
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+
+  # 空の商品出品を保存できないようにする
+  validates :name, :description, presence: true
+  # ジャンルの選択が「---」の時は保存できないようにする
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
 end
