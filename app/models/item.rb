efc2403f08b7435c9f313f04714779e5f9b5e6cd
lazykapo <sideorder921@gmail.com>
2024-10-21 16:42:08 +0900

    修正2

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,21 +10,17 @@ class Item < ApplicationRecord
   belongs_to :shipping_day
 
   with_options presence: true do
+    validates :user_id
     validates :name
     validates :description
     validates :image
-    validates :category_id
-    validates :condition_id
-    validates :shipping_cost_id
-    validates :prefecture_id
-    validates :shipping_day_id
     # 300円以上かつ9,999,999円以下で、半角数字でないと入力不可
     validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   end
 
-  validates :category_id, numericality: { other_than: 1, message: "can't be blank"}
-  validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :shipping_cost_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :shipping_day_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :category_id, numericality: { other_than: 1, message: "must be other than 1"}
+  validates :condition_id, numericality: { other_than: 1, message: "must be other than 1" }
+  validates :shipping_cost_id, numericality: { other_than: 1, message: "must be other than 1" }
+  validates :prefecture_id, numericality: { other_than: 1, message: "must be other than 1" }
+  validates :shipping_day_id, numericality: { other_than: 1, message: "must be other than 1" }
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -13,8 +13,13 @@ RSpec.describe Item, type: :model do
     end
 
     context '商品出品がうまくいかないとき' do
+      it 'ユーザー登録している人でないと登録されない' do
+        @item.user_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include('User must exist', "User can't be blank")
+      end
       it 'imageが空では登録されない' do
-        @item.image = ''
+        @item.image = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Image can't be blank")
       end
@@ -28,30 +33,30 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Description can't be blank")
       end
-      it 'category_idが空では登録されない' do
-        @item.category_id = nil
+      it 'category_idは「---」が選択されているときは登録されない' do
+        @item.category_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Category can't be blank")
+        expect(@item.errors.full_messages).to include("Category must be other than 1")
       end
-      it 'condition_idが空では登録されない' do
-        @item.condition_id = nil
+      it 'condition_idは「---」が選択されているときは登録されない' do
+        @item.condition_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Condition can't be blank")
+        expect(@item.errors.full_messages).to include("Condition must be other than 1")
       end
-      it 'shipping_cost_idが空では登録されない' do
-        @item.shipping_cost_id = nil
+      it 'shipping_cost_idは「---」が選択されているときは登録されない' do
+        @item.shipping_cost_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Shipping cost can't be blank")
+        expect(@item.errors.full_messages).to include("Shipping cost must be other than 1")
       end
-      it 'prefecture_idが空では登録されない' do
-        @item.prefecture_id = nil
+      it 'prefecture_idは「---」が選択されているときは登録されない' do
+        @item.prefecture_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+        expect(@item.errors.full_messages).to include("Prefecture must be other than 1")
       end
-      it 'shipping_day_idが空では登録されない' do
-        @item.shipping_day_id = nil
+      it 'shipping_day_idは「---」が選択されているときは登録されない' do
+        @item.shipping_day_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Shipping day can't be blank")
+        expect(@item.errors.full_messages).to include("Shipping day must be other than 1")
       end
       it 'priceが空では登録されない' do
         @item.price = ""
