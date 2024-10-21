9f7a651ef42d0d012cc077ea14370548e5069907
lazykapo <sideorder921@gmail.com>
2024-10-21 15:29:00 +0900

    修正1

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
-  has_one :order
+# has_one :order
   has_one_attached :image
   belongs_to :category
   belongs_to :condition 
@@ -19,7 +19,7 @@ class Item < ApplicationRecord
     validates :prefecture_id
     validates :shipping_day_id
     # 300円以上かつ9,999,999円以下で、半角数字でないと入力不可
-    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   end
 
   validates :category_id, numericality: { other_than: 1, message: "can't be blank"}
