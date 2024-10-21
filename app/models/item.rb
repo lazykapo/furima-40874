ede05406245ce24d775062fb56e950284c65b31c
lazykapo <sideorder921@gmail.com>
2024-10-21 11:11:01 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,22 +1,30 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
   has_one :order
   has_one_attached :image
-
-  # ここから下は商品名と内容が画像に必須かどうか
-  # validates :name, presence: true, unless: :was_attached?
-  # validates :description, presence: true, unless: :was_attached?
-
-  # def was_attached?
-  # image.attached?
-  # end
-
-  # Itemモデルのアソシエーションを設定
-  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  belongs_to :condition 
+  belongs_to :shipping_cost
+  belongs_to :prefecture
+  belongs_to :shipping_day
 
-  # 空の商品出品を保存できないようにする
-  validates :name, :description, presence: true
-  # ジャンルの選択が「---」の時は保存できないようにする
-  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :image
+    validates :category_id
+    validates :condition_id
+    validates :shipping_cost_id
+    validates :prefecture_id
+    validates :shipping_day_id
+    # 300円以上かつ9,999,999円以下で、半角数字でないと入力不可
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  end
+
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_cost_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_day_id, numericality: { other_than: 1, message: "can't be blank" }
 end
