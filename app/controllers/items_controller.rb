2a6c3c588f206fa1fcdd4d723e1c16de7564146d
lazykapo <sideorder921@gmail.com>
2024-10-29 11:55:34 +0900

    商品詳細機能の条件分岐

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,7 @@ class ItemsController < ApplicationController
   before_action :move_to_index, only: [:edit, :update]
 
   def index
-    @items = Item.includes(:user).order("created_at DESC")
+    @items = Item.includes(:user).order('created_at DESC')
   end
 
   def new
@@ -13,7 +13,7 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    if @item.valid? 
+    if @item.valid?
       @item.save
       redirect_to root_path
     else
@@ -22,18 +22,19 @@ class ItemsController < ApplicationController
   end
 
   def show
-    
   end
 
   def edit
+    return if current_user && @item.order.nil?
 
+    redirect_to root_path
   end
 
   def update
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
-      render :edit, status: :unprocessable_entity  
+      render :edit, status: :unprocessable_entity
     end
   end
 
@@ -47,15 +48,15 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:name, :description, :image, :category_id, :condition_id, :shipping_cost_id, :prefecture_id,
                                  :shipping_day_id, :price).merge(user_id: current_user.id)
-  end 
+  end
 
   def set_item
     @item = Item.find(params[:id])
   end
 
   def move_to_index
-    unless current_user.id == @item.user_id
-      redirect_to action: :index
-    end
+    return if current_user.id == @item.user_id
+
+    redirect_to action: :index
   end
 end
